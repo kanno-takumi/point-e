7023b9d6afb1ba7fe8fec750e4377d4595616f64
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-11-30 02:57:25 +0900

    作ったポリゴンメッシュを表示する機能を追加

---
+++ read_ply.py
@@ -0,0 +1,19 @@
+import open3d as o3d
+import numpy as np
+
+if __name__ == "__main__":
+    
+    # Loading point cloud
+    print("Loading point cloud")
+    #ptCloud = o3d.io.read_point_cloud("dataset/trimesh_primitives/train/0/box_0000.ply")
+    ptCloud = o3d.io.read_point_cloud("mesh.ply")
+
+    # confirmation
+    print(ptCloud)
+    print(np.asarray(ptCloud.points))
+    
+    # Visualization in window
+    o3d.visualization.draw_geometries([ptCloud])
+    
+    # Saving point cloud
+    #o3d.io.write_point_cloud("output.ply", ptCloud)　#すでに保存してあるため必要ない
