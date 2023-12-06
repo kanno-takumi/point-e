b5a3944d28b552e06ade791a10e89cd58cf16612
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-12-07 01:45:11 +0900

    修正

--- pc2mesh.py
+++ pc2mesh.py
@@ -6,7 +6,7 @@ from tqdm.auto import tqdm
 
 from point_e.models.download import load_checkpoint
 from point_e.models.configs import MODEL_CONFIGS, model_from_config
-from point_e.util.pc2mesh import marching_cubes_mesh
+from point_e.util.pc_to_mesh import marching_cubes_mesh
 from point_e.util.plotting import plot_point_cloud
 from point_e.util.point_cloud import PointCloud
 import json
